8e148e77835c280f1a1ad5e84660140c1bc8a8fb
devmateusramos <devmateusramos@gmail.com>
2021-12-09 19:47:08 -0300

    pilhas intro

---
+++ 3- Pilhas/1.intro.js
@@ -0,0 +1,31 @@
+function stack() {
+  var items = [];
+
+  this.push = function() {
+      // Adiciona novo item à pilha
+  }
+
+  this.pop = function() {
+      // Remover item que está no topo da pilha
+  }
+
+  this.peek = function() {
+      // Devolve o elemento que está no topo da pilha
+  }
+
+  this.isEmpty = function() {
+      // Informar se a pilha está vazia ou não
+  }
+
+  this.clear = function() {
+      // Limpa a pilha
+  }
+
+  this.size = function() {
+      // Informar o tamanho da pilha
+  }
+
+  this.print() = function() {
+      // Imprime a pilha no console
+  }
+}
